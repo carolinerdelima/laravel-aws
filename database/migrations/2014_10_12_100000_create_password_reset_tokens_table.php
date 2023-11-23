ff082968cd65071a2d93817d39b38e22876f4654
caroline.lima <caroline.lima@kinghost.com.br>
2023-11-23 16:16:04 -0300

    Ajustando migrations para dropar antes

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+        $this->down();
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-        });
+            $table->primary(['email', 'token']);
+        });      
     }
 
     /**
